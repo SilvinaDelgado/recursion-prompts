38cfd76a988b89598ccf9296f6effc623ec90627
Silvina Delgado <silvina.ruiz@gmail.com>
2018-07-28 15:56:53 -0500

    Problem 6 Range

--- src/recursion.js
+++ src/recursion.js
@@ -64,7 +64,18 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if(x === y || x + 1 === y){
+    return [];
+  }
+  if(x > y ){
+    if(x - 1 === y){
+      return [];
+    }
+    return [x - 1].concat(range(x - 1, y));
+  }
+return [x +  1].concat(range(x + 1, y));
 };
+
 
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
@@ -72,6 +83,7 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  
 };
 
 // 8. Determine if a number is a power of two.
